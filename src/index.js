c05de3207c53fa56dd4a4931be19c98b6913985e
PinkBlink <PinkBlink@123.home>
2023-02-02 14:36:11 +0100

    add: complete decoder

--- src/index.js
+++ src/index.js
@@ -35,12 +35,80 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    
+};
+const DOTS={
+    '10': '.',
+    '11': '-',
+    '**': '**',
 };
 
-function decode(expr) {
-    // write your solution here
+
+
+
+//делит числа на слова и переводит в морзе
+function morseWords(letters){
+    let result = [];
+    let words = [];
+
+    for(let i=0 ; i<letters.length ; i+=10){
+        words.push(letters.substring( i , i+10 ));
+    }
+
+    for(let num of words) {
+
+        let word = [];
+
+        for(let i=0 ; i<num.length ; i+=2) {
+
+        let letter = num.substring(i,i+2);
+
+        if ( letter == '00' ) {
+            continue;
+        } else {
+            word.push( DOTS[letter] );
+        }
+    
+    }
+
+    result.push( word.join('') );
+
+    }
+
+    return result;
 }
+
+//переводит морзе в слова;
+function decode(expr) {
+
+ let result=[];
+
+ let phrase=morseWords(expr);
+
+ for ( let dot of phrase ) {
+
+    if ( dot == '**********' ) {
+        
+        result.push(' '); 
+
+    } else if ( dot==' ' || dot==',' ) {
+        
+        continue;
+
+    } else {
+
+        result.push( MORSE_TABLE[dot] );
+
+    }
+    
+ }
+ return result.join('');
+}
+
 
 module.exports = {
     decode
 }
+
+
+
